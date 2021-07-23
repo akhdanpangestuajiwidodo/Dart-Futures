d441bd8a835b73922cbc627221ef86366776be1a
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-07-24 05:27:50 +0700

    Initial Commit

---
+++ bin/dart_futures.dart
@@ -0,0 +1,5 @@
+import 'package:dart_futures/dart_futures.dart' as dart_futures;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_futures.calculate()}!');
+}
---
+++ test/dart_futures_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_futures/dart_futures.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
